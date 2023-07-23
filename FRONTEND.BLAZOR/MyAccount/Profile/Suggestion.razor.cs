96eaa228ca35f696140c2a501f6720b86c0a4f57
abidalisayyed786@gmail.com <abidalisayyed786@gmail.com>
2023-07-23 23:34:49 +0530

    [MYIN-31] - Add the static pages Offers, Suggestion, Complaint, Notification, Chat and History As per given updated UI

---
+++ FRONTEND.BLAZOR/MyAccount/Profile/Suggestion.razor.cs
@@ -0,0 +1,37 @@
+﻿using BAL.Services.Contracts;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Threading.Tasks;
+
+namespace FRONTEND.BLAZOR.MyAccount.Profile
+{
+    public partial class Suggestion
+    {
+        [Inject]
+        public IUserService userService { get; set; }
+        [Inject]
+        AuthenticationStateProvider authenticationState { get; set; }
+
+        public bool isVendor { get; set; } = false;
+
+        protected async override Task OnInitializedAsync()
+        {
+            try
+            {
+                var authstate = await authenticationState.GetAuthenticationStateAsync();
+                var user = authstate.User;
+                if (user.Identity.IsAuthenticated)
+                {
+                    var applicationUser = await userService.GetUserByUserName(user.Identity.Name);
+                    string CurrentUserGuid = applicationUser.Id;
+                    isVendor = applicationUser.IsVendor;
+                }
+            }
+            catch (Exception exc)
+            {
+
+            }
+        }
+    }
+}
