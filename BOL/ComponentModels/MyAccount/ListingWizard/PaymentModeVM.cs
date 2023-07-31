75dd1cb7d7396d9aebde017dfe9907ebdb58e459
abidalisayyed786@gmail.com <abidalisayyed786@gmail.com>
2023-07-31 22:53:32 +0530

    [MYIN-44] - Merge PaymentModeEdit and PaymentMode page and code refactoring

---
+++ BOL/ComponentModels/MyAccount/ListingWizard/PaymentModeVM.cs
@@ -0,0 +1,64 @@
+﻿using BOL.LISTING;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOL.ComponentModels.MyAccount.ListingWizard
+{
+    public class PaymentModeVM
+    {
+        public bool Cash { get; set; } = false;
+        public bool NetBanking { get; set; }
+        public bool Cheque { get; set; } = false;
+        public bool RtgsNeft { get; set; }
+        public bool DebitCard { get; set; }
+        public bool CreditCard { get; set; }
+        public bool PayTM { get; set; }
+        public bool PhonePay { get; set; }
+        public bool Paypal { get; set; }
+
+        public void SetViewModel(PaymentMode paymentMode)
+        {
+            Cash = paymentMode.Cash;
+            NetBanking = paymentMode.NetBanking;
+            Cheque = paymentMode.Cheque;
+            RtgsNeft = paymentMode.RtgsNeft;
+            DebitCard = paymentMode.DebitCard;
+            CreditCard = paymentMode.CreditCard;
+            PayTM = paymentMode.PayTM;
+            PhonePay = paymentMode.PhonePay;
+            Paypal = paymentMode.Paypal;
+        }
+
+        public void SetPaymentMode(PaymentMode paymentMode)
+        {
+            paymentMode.Cash = Cash;
+            paymentMode.NetBanking = NetBanking;
+            paymentMode.Cheque = Cheque;
+            paymentMode.RtgsNeft = RtgsNeft;
+            paymentMode.DebitCard = DebitCard;
+            paymentMode.CreditCard = CreditCard;
+            paymentMode.PayTM = PayTM;
+            paymentMode.PhonePay = PhonePay;
+            paymentMode.Paypal = Paypal;
+        }
+
+        public void selectOrUnselectAll(bool isSelect = true)
+        {
+            Cash = isSelect;
+            NetBanking = isSelect;
+            Cheque = isSelect;
+            RtgsNeft = isSelect;
+            DebitCard = isSelect;
+            CreditCard = isSelect;
+            PayTM = isSelect;
+            PhonePay = isSelect;
+            Paypal = isSelect;
+        }
+
+        public bool isValid()
+        {
+            return Cash || NetBanking || Cheque || RtgsNeft || DebitCard || CreditCard || PayTM || PhonePay || Paypal;
+        }
+    }
+}
--- FRONTEND.BLAZOR/MyAccount/ListingWizard/PaymentMode.razor.cs
+++ FRONTEND.BLAZOR/MyAccount/ListingWizard/PaymentMode.razor.cs
@@ -1,357 +1,33 @@
 ﻿using System;
-using System.Linq;
 using System.Threading.Tasks;
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using FRONTEND.BLAZOR.Services;
 using AntDesign;
-using BOL.CATEGORIES;
-using BOL.LISTING;
-using DAL.Models;
 using BAL.Services.Contracts;
+using BOL.ComponentModels.MyAccount.ListingWizard;
+using BAL;
 
 namespace FRONTEND.BLAZOR.MyAccount.ListingWizard
 {
     public partial class PaymentMode
     {
         [Inject]
-        public IUserService userService { get; set; }
-
-        // Begin: Check if record exisit with listingId
-        [Parameter]
-        public int? listingId { get; set; }
-
-        public string currentPage = "nav-paymentmode";
-        public bool buttonBusy { get; set; }
-
-        [Inject]
         private IHttpContextAccessor httpConAccess { get; set; }
+        [Inject]
+        IUserService userService { get; set; }
+        [Inject]
+        IListingService listingService { get; set; }
+        [Inject]
+        Helper helper { get; set; }
+
+        PaymentModeVM PaymentModeVM { get; set; } = new PaymentModeVM();
+
+        public bool buttonBusy { get; set; }
         public string CurrentUserGuid { get; set; }
-        public string ErrorMessage { get; set; }
-        public bool userAuthenticated { get; set; } = false;
-        public string IpAddress { get; set; }
-        public ApplicationUser iUser { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public DateTime CreatedTime { get; set; }
+        public int ListingId { get; set; }
+        public bool IsWorkingHourExist { get; set; }
 
-        // Begin: Check if record exists
-        public bool companyExist { get; set; }
-        public bool communicationExist { get; set; }
-        public bool addressExist { get; set; }
-        public bool categoryExist { get; set; }
-        public bool specialisationExist { get; set; }
-        public bool workingHoursExist { get; set; }
-        public bool paymentModeExist { get; set; }
-
-        public async Task CompanyExistAsync()
-        {
-            if (listingId != null)
-            {
-                var company = await listingContext.Listing.Where(i => i.ListingID == listingId).FirstOrDefaultAsync();
-
-                if (company != null)
-                {
-                    companyExist = true;
-                }
-                else
-                {
-                    companyExist = false;
-                }
-            }
-        }
-
-        public async Task CommunicationExistAsync()
-        {
-            if (listingId != null)
-            {
-                var communication = await listingContext.Communication.Where(i => i.ListingID == listingId).FirstOrDefaultAsync();
-
-                if (communication != null)
-                {
-                    communicationExist = true;
-                }
-                else
-                {
-                    communicationExist = false;
-                }
-            }
-        }
-
-        public async Task AddressExistAsync()
-        {
-            if (listingId != null)
-            {
-                var address = await listingContext.Address.Where(i => i.ListingID == listingId).FirstOrDefaultAsync();
-
-                if (address != null)
-                {
-                    addressExist = true;
-                }
-                else
-                {
-                    addressExist = false;
-                }
-            }
-        }
-
-        public async Task CategoryExistAsync()
-        {
-            if (listingId != null)
-            {
-                var category = await listingContext.Categories.Where(i => i.ListingID == listingId).FirstOrDefaultAsync();
-
-                if (category != null)
-                {
-                    categoryExist = true;
-                }
-                else
-                {
-                    categoryExist = false;
-                }
-            }
-        }
-
-        public async Task SpecialisationExistAsync()
-        {
-            if (listingId != null)
-            {
-                var specialisation = await listingContext.Specialisation.Where(i => i.ListingID == listingId).FirstOrDefaultAsync();
-
-                if (specialisation != null)
-                {
-                    specialisationExist = true;
-                }
-                else
-                {
-                    specialisationExist = false;
-                }
-            }
-        }
-
-        public async Task WorkingHoursExistAsync()
-        {
-            if (listingId != null)
-            {
-                var wh = await listingContext.WorkingHours.Where(i => i.ListingID == listingId).FirstOrDefaultAsync();
-
-                if (wh != null)
-                {
-                    workingHoursExist = true;
-                }
-                else
-                {
-                    workingHoursExist = false;
-                }
-            }
-        }
-
-        public async Task PaymentModeExistAsync()
-        {
-            if (listingId != null)
-            {
-                var pm = await listingContext.PaymentMode.Where(i => i.ListingID == listingId).FirstOrDefaultAsync();
-
-                if (pm != null)
-                {
-                    paymentModeExist = true;
-                }
-                else
-                {
-                    paymentModeExist = false;
-                }
-            }
-        }
-        // End: Check if record exists
-
-        // Properties
-        public bool Cash { get; set; } = false;
-        public bool NetBanking { get; set; }
-        public bool Cheque { get; set; } = false;
-        public bool RtgsNeft { get; set; }
-        public bool DebitCard { get; set; }
-        public bool CreditCard { get; set; }
-        public bool PayTM { get; set; }
-        public bool PhonePay { get; set; }
-        public bool Paypal { get; set; }
-
-        public bool SelectAll { get; set; }
-
-        public async Task ToggleAllAsync()
-        {
-            SelectAll = !SelectAll;
-
-            if(SelectAll == true)
-            {
-                Cash = true;
-                NetBanking = true;
-                Cheque = true;
-                RtgsNeft = true;
-                DebitCard = true;
-                CreditCard = true;
-                PayTM = true;
-                PhonePay = true;
-                Paypal = true;
-            }
-            else
-            {
-                Cash = false;
-                NetBanking = false;
-                Cheque = false;
-                RtgsNeft = false;
-                DebitCard = false;
-                CreditCard = false;
-                PayTM = false;
-                PhonePay = false;
-                Paypal = false;
-            }
-            
-
-            await Task.Delay(1);
-        }
-
-        // Begin: Create Payment Mode
-        public async Task CreatePaymentModeAsync()
-        {
-            buttonBusy = true;
-
-            // Check if specialisation already exists
-            var duplicate = await listingContext.PaymentMode.Where(i => i.ListingID == listingId).FirstOrDefaultAsync();
-
-            try
-            {
-                if (duplicate == null)
-                {
-                    try
-                    {
-                        await CountPropertyAsync();
-                        if(propertyCount > 0)
-                        {
-                            BOL.LISTING.PaymentMode pm = new BOL.LISTING.PaymentMode
-                            {
-                                ListingID = listingId.Value,
-                                OwnerGuid = CurrentUserGuid,
-                                IPAddress = IpAddress,
-                                Cash = Cash,
-                                NetBanking = NetBanking,
-                                Cheque = Cheque,
-                                RtgsNeft = RtgsNeft,
-                                DebitCard = DebitCard,
-                                CreditCard = CreditCard,
-                                PayTM = PayTM,
-                                PhonePay = PhonePay,
-                                Paypal = Paypal
-                            };
-
-                            await listingContext.AddAsync(pm);
-                            await listingContext.SaveChangesAsync();
-
-                            // Navigate To
-                            navManager.NavigateTo($"/MyAccount/ListingWizard/Logo/{listingId}");
-                        }
-                        else
-                        {
-                            // Show notification
-                            await NoticeWithIcon(NotificationType.Error, NotificationPlacement.BottomRight, "Error", "Please select at least one payment mode.");
-
-                            buttonBusy = false;
-                        }
-                    }
-                    catch(Exception exc)
-                    {
-                        // Show notification
-                        await NoticeWithIcon(NotificationType.Success, NotificationPlacement.BottomRight, "Error", exc.Message);
-
-                        buttonBusy = false;
-                    }
-                }
-                else
-                {
-                    // Show notification
-                    await NoticeWithIcon(NotificationType.Success, NotificationPlacement.BottomRight, "Error", $"Payment Mode for Listing ID {duplicate.ListingID} already exists.");
-
-                    buttonBusy = false;
-                }
-            }
-            catch(Exception exc)
-            {
-                // Show notification
-                await NoticeWithIcon(NotificationType.Success, NotificationPlacement.BottomRight, "Error", exc.Message);
-
-                buttonBusy = false;
-            }
-        }
-        // End: Create Specialisation
-
-        // Begin: Antdesign Blazor Notification
-        private async Task NoticeWithIcon(NotificationType type, NotificationPlacement placement, string message, string description)
-        {
-            await _notice.Open(new NotificationConfig()
-            {
-                Message = message,
-                Description = description,
-                NotificationType = type,
-                Placement = placement
-            });
-        }
-        // End: Antdesign Blazor Notification
-
-        // Begin: Count Properties
-        public int propertyCount = 0;
-        public async Task<int> CountPropertyAsync()
-        {
-            if (Cash == true)
-            {
-                propertyCount++;
-            }
-
-            if (NetBanking == true)
-            {
-                propertyCount++;
-            }
-
-            if (Cheque == true)
-            {
-                propertyCount++;
-            }
-
-            if (RtgsNeft == true)
-            {
-                propertyCount++;
-            }
-
-            if (DebitCard == true)
-            {
-                propertyCount++;
-            }
-
-            if (CreditCard == true)
-            {
-                propertyCount++;
-            }
-
-            if (PayTM == true)
-            {
-                propertyCount++;
-            }
-
-            if (PhonePay == true)
-            {
-                propertyCount++;
-            }
-
-            if (Paypal == true)
-            {
-                propertyCount++;
-            }
-
-            await Task.Delay(10);
-            return propertyCount;
-        }
-        // End: Count Properties
-
+ 
         protected async override Task OnInitializedAsync()
         {
             try
@@ -361,38 +37,83 @@ namespace FRONTEND.BLAZOR.MyAccount.ListingWizard
                 var user = authstate.User;
                 if (user.Identity.IsAuthenticated)
                 {
-                    // Shafi: Assign Time Zone to CreatedDate & Created Time
-                    DateTime timeZoneDate = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById("India Standard Time"));
-                    IpAddress = httpConAccess.HttpContext.Connection.RemoteIpAddress.ToString();
-                    CreatedDate = timeZoneDate;
-                    CreatedTime = timeZoneDate;
-                    // End:
+                    var applicationUser = await userService.GetUserByUserName(user.Identity.Name);
+                    CurrentUserGuid = applicationUser.Id;
+                    var listing = await listingService.GetListingByOwnerId(CurrentUserGuid);
+                    if (listing == null)
+                        navManager.NavigateTo("/MyAccount/Listing/Company");
+                    else if (listing.Steps < Constants.WorkingHourComplete) //Checking if prev steps compeleted
+                        helper.NavigateToPageByStep(listing.Steps, navManager);
 
-                    iUser = await userService.GetUserByUserName(user.Identity.Name);
-                    CurrentUserGuid = iUser.Id;
-
-                    userAuthenticated = true;
-
-                    // Begin: Check if record exists
-                    await CompanyExistAsync();
-                    await CommunicationExistAsync();
-                    await AddressExistAsync();
-                    await CategoryExistAsync();
-                    await SpecialisationExistAsync();
-                    await WorkingHoursExistAsync();
-                    await PaymentModeExistAsync();
-                    // End: Check if record exists
-
-                    if (paymentModeExist == true)
+                    ListingId = listing.ListingID;
+                    var paymentMode = await listingService.GetPaymentModeByListingId(ListingId);
+                    if (paymentMode != null)
                     {
-                        string url = "/MyAccount/ListingWizard/PaymentModeEdit/" + listingId;
-                        navManager.NavigateTo(url);
+                        IsWorkingHourExist = true;
+                        PaymentModeVM.SetViewModel(paymentMode);
                     }
                 }
             }
             catch (Exception exc)
             {
-                ErrorMessage = exc.Message;
+                string ErrorMessage = exc.Message;
+            }
+        }
+
+        public async Task ToggleAllAsync()
+        {
+            PaymentModeVM.selectOrUnselectAll();
+            await Task.Delay(1);
+        }
+
+        public async Task AddOrUpdatePaymentMode()
+        {
+            if (!PaymentModeVM.isValid())
+            {
+                await helper.ShowNotification(_notice, NotificationType.Error, NotificationPlacement.BottomRight, "Error", "Please select at least one payment mode.");
+                return;
+            }
+
+            try
+            {
+                buttonBusy = true;
+                var paymentMode = await listingService.GetPaymentModeByListingId(ListingId);
+                bool recordNotFound = paymentMode == null;
+
+                if (recordNotFound)
+                    paymentMode = new BOL.LISTING.PaymentMode();
+
+                PaymentModeVM.SetPaymentMode(paymentMode);
+
+                if (recordNotFound)
+                {
+                    paymentMode.ListingID = ListingId;
+                    paymentMode.OwnerGuid = CurrentUserGuid;
+                    paymentMode.IPAddress = httpConAccess.HttpContext.Connection.RemoteIpAddress.ToString();
+
+                    await listingService.AddAsync(paymentMode);
+                }
+                else
+                {
+                    await listingService.UpdateAsync(paymentMode);
+                }
+
+                var listing = await listingService.GetListingByOwnerId(CurrentUserGuid);
+                if (listing.Steps < Constants.PaymentModeComplete)
+                {
+                    listing.Steps = Constants.PaymentModeComplete;
+                    await listingService.UpdateAsync(listing);
+                }
+
+                //navManager.NavigateTo("/MyAccount/Listing/PaymentMode");
+            }
+            catch (Exception exc)
+            {
+                await helper.ShowNotification(_notice, NotificationType.Error, NotificationPlacement.BottomRight, "Error", exc.Message);
+            }
+            finally
+            {
+                buttonBusy = false;
             }
         }
     }
