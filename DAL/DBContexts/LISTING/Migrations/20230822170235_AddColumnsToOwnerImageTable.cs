44ca9c83c7c0ec119911faf4fdb6af2d28acc78c
abidalisayyed786@gmail.com <abidalisayyed786@gmail.com>
2023-08-22 22:38:50 +0530

    updated columns in database into ownerimage table

---
+++ DAL/DBContexts/LISTING/Migrations/20230822170235_AddColumnsToOwnerImageTable.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.DBContexts.LISTING.Migrations
+{
+    public partial class AddColumnsToOwnerImageTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Cast",
+                table: "OwnerImage",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CountryID",
+                table: "OwnerImage",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Religion",
+                table: "OwnerImage",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "StateID",
+                table: "OwnerImage",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cast",
+                table: "OwnerImage");
+
+            migrationBuilder.DropColumn(
+                name: "CountryID",
+                table: "OwnerImage");
+
+            migrationBuilder.DropColumn(
+                name: "Religion",
+                table: "OwnerImage");
+
+            migrationBuilder.DropColumn(
+                name: "StateID",
+                table: "OwnerImage");
+        }
+    }
+}
