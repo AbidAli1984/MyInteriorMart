9d19033acbc7920a76209a72e5dd54f64a07128d
abidalisayyed786@gmail.com <abidalisayyed786@gmail.com>
2023-08-30 23:24:42 +0530

    Prepopulate fields for get quotes functionality and allow only loggedin user to get quotes if user is not logged in display a pop with login/register link.

--- FRONTEND.BLAZOR/Modal/ListingEnquiryModal.razor.cs
+++ FRONTEND.BLAZOR/Modal/ListingEnquiryModal.razor.cs
@@ -15,12 +15,34 @@ namespace FRONTEND.BLAZOR.Modal
         [Parameter] public string CurrentUserGuid { get; set; }
         [Parameter] public int ListingId { get; set; }
 
+        [Inject] private IUserService userService { get; set; }
+        [Inject] private IUserProfileService userProfileService { get; set; }
         [Inject] private IListingService listingService { get; set; }
         [Inject] Helper helper { get; set; }
+        [Inject] NavigationManager navigationManager { get; set; }
 
         public bool showEnquiryModal { get; set; } = false;
+        private bool isRegisteredUser { get; set; } = false;
 
         public ListingEnquiryVM ListingEnquiryVM { get; set; } = new ListingEnquiryVM();
+
+        protected async override void OnAfterRender(bool firstRender)
+        {
+            if (!string.IsNullOrWhiteSpace(CurrentUserGuid))
+            {
+                var applicationUser = await userService.GetUserById(CurrentUserGuid);
+                isRegisteredUser = applicationUser != null;
+
+                if (isRegisteredUser)
+                {
+                    var userProfile = await userProfileService.GetProfileByOwnerGuid(CurrentUserGuid);
+                    if (userProfile != null)
+                        ListingEnquiryVM.ListingEnquiry.FullName = userProfile.Name;
+                    ListingEnquiryVM.ListingEnquiry.Email = applicationUser.Email;
+                    ListingEnquiryVM.ListingEnquiry.MobileNumber = applicationUser.PhoneNumber;
+                }
+            }
+        }
 
         public async Task HideEnquiryModal()
         {
@@ -34,8 +56,23 @@ namespace FRONTEND.BLAZOR.Modal
             await Task.Delay(5);
         }
 
+        private void NavigateToLogin()
+        {
+            showEnquiryModal = false;
+            navigationManager.NavigateTo("/Auth/Login", true);
+        }
+
+        private void NavigateToRegister()
+        {
+            showEnquiryModal = false;
+            navigationManager.NavigateTo("/Auth/Register", true);
+        }
+
         public async Task CreateEnquiry()
         {
+            if (!!isRegisteredUser)
+                return;
+
             if (!ListingEnquiryVM.isValid())
             {
                 helper.ShowNotification(_notice, NotificationType.Error, NotificationPlacement.BottomRight, "Error", $"All Fields are mandatory!.");
