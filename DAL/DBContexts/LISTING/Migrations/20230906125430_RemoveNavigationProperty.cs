a9065d4c05fcd907f24ce43210e8e6224c89a1f5
abidalisayyed786@gmail.com <abidalisayyed786@gmail.com>
2023-09-06 19:23:07 +0530

    Navigation property replace

---
+++ DAL/DBContexts/LISTING/Migrations/20230906125430_RemoveNavigationProperty.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.DBContexts.LISTING.Migrations
+{
+    public partial class RemoveNavigationProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RatingReply_Rating_RatingId",
+                table: "RatingReply");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RatingReply_RatingId",
+                table: "RatingReply");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_RatingReply_RatingId",
+                table: "RatingReply",
+                column: "RatingId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RatingReply_Rating_RatingId",
+                table: "RatingReply",
+                column: "RatingId",
+                principalSchema: "listing",
+                principalTable: "Rating",
+                principalColumn: "RatingID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ DAL/DBContexts/LISTING/Migrations/20230906131336_AddNavigationProperty.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.DBContexts.LISTING.Migrations
+{
+    public partial class AddNavigationProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_RatingReply_RatingId",
+                table: "RatingReply",
+                column: "RatingId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RatingReply_Rating_RatingId",
+                table: "RatingReply",
+                column: "RatingId",
+                principalSchema: "listing",
+                principalTable: "Rating",
+                principalColumn: "RatingID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RatingReply_Rating_RatingId",
+                table: "RatingReply");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RatingReply_RatingId",
+                table: "RatingReply");
+        }
+    }
+}
