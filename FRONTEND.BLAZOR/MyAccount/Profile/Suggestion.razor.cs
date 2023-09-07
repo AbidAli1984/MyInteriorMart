d3d21cf6fdcf084e0e2df4650893952c71860e5a
abidalisayyed786@gmail.com <abidalisayyed786@gmail.com>
2023-09-07 15:35:24 +0530

    Suggestion Implementation done

--- FRONTEND.BLAZOR/MyAccount/Profile/Suggestion.razor.cs
+++ FRONTEND.BLAZOR/MyAccount/Profile/Suggestion.razor.cs
@@ -1,4 +1,6 @@
-﻿using BAL.Services.Contracts;
+﻿using AntDesign;
+using BAL.Services.Contracts;
+using BOL.AUDITTRAIL;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using System;
@@ -8,11 +10,16 @@ namespace FRONTEND.BLAZOR.MyAccount.Profile
 {
     public partial class Suggestion
     {
-        [Inject]
-        public IUserService userService { get; set; }
-        [Inject]
-        AuthenticationStateProvider authenticationState { get; set; }
+        [Inject] public IAuditService auditService { get; set; }
+        [Inject] public IUserService userService { get; set; }
+        [Inject] public IUserProfileService userProfileService { get; set; }
+        [Inject] AuthenticationStateProvider authenticationState { get; set; }
+        [Inject] NotificationService _notification { get; set; }
+        [Inject] Helper helper { get; set; }
 
+        public Suggestions Suggestions { get; set; } = new Suggestions();
+
+        public bool buttonBusy { get; set; }
         public bool isVendor { get; set; } = false;
 
         protected async override Task OnInitializedAsync()
@@ -24,14 +31,54 @@ namespace FRONTEND.BLAZOR.MyAccount.Profile
                 if (user.Identity.IsAuthenticated)
                 {
                     var applicationUser = await userService.GetUserByUserName(user.Identity.Name);
-                    string CurrentUserGuid = applicationUser.Id;
+                    var userProfile = await userProfileService.GetProfileByOwnerGuid(applicationUser.Id);
                     isVendor = applicationUser.IsVendor;
+
+                    Suggestions.OwnerGuid = applicationUser.Id;
+                    Suggestions.Mobile = applicationUser.PhoneNumber;
+                    Suggestions.Email = applicationUser.Email;
+                    Suggestions.Date = DateTime.Now;
+
+                    if (userProfile != null)
+                        Suggestions.Name = userProfile.Name;
                 }
             }
             catch (Exception exc)
             {
 
             }
+        }
+
+        private async Task AddSuggestion()
+        {
+            if (string.IsNullOrEmpty(Suggestions.Title) || string.IsNullOrEmpty(Suggestions.Suggestion))
+            {
+                helper.ShowNotification(_notification, NotificationType.Info, NotificationPlacement.BottomRight, "Information", "All fields are compulsory.");
+                return;
+            }
+
+            try
+            {
+                buttonBusy = true;
+                await auditService.AddAsync(Suggestions);
+                ResetSuggestion();
+                helper.ShowNotification(_notification, NotificationType.Success, NotificationPlacement.BottomRight, "Success", "Your suggestion submitted successfully!");
+            }
+            catch (Exception exc)
+            {
+                helper.ShowNotification(_notification, NotificationType.Error, NotificationPlacement.BottomRight, "Error", exc.Message);
+            }
+            finally
+            {
+                buttonBusy = false;
+            }
+        }
+
+        private void ResetSuggestion()
+        {
+            Suggestions.SuggestionID = 0;
+            Suggestions.Title = string.Empty;
+            Suggestions.Suggestion = string.Empty;
         }
     }
 }
