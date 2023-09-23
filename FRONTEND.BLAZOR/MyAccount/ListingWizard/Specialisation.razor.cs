24e142cfc361f12e22508d3d0c18cb83f172a2ef
abidalisayyed786@gmail.com <abidalisayyed786@gmail.com>
2023-09-23 17:42:29 +0530

    SpecialisationForm component impelemented for reuseability

--- FRONTEND.BLAZOR/MyAccount/ListingWizard/Specialisation.razor.cs
+++ FRONTEND.BLAZOR/MyAccount/ListingWizard/Specialisation.razor.cs
@@ -6,30 +6,13 @@ using AntDesign;
 using BAL.Services.Contracts;
 using BOL;
 using BOL.ComponentModels.MyAccount.ListingWizard;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace FRONTEND.BLAZOR.MyAccount.ListingWizard
 {
     public partial class Specialisation
     {
-        [Inject]
-        private IHttpContextAccessor httpConAccess { get; set; }
-        [Inject]
-        private ISharedService sharedService { get; set; }
-        [Inject]
-        IUserService userService { get; set; }
-        [Inject]
-        IListingService listingService { get; set; }
-        [Inject]
-        Helper helper { get; set; }
-
-        public SpecialisationVM SpecialisationVM { get; set; } = new SpecialisationVM();
-
-        public bool buttonBusy { get; set; }
-        public string CurrentUserGuid { get; set; }
-        public int ListingId { get; set; }
-        public int Steps { get; set; }
-        public bool IsSpecialisationExist { get; set; }
-
+        [Inject] AuthenticationStateProvider authenticationState { get; set; }
 
         protected async override Task OnInitializedAsync()
         {
@@ -40,75 +23,12 @@ namespace FRONTEND.BLAZOR.MyAccount.ListingWizard
                 var user = authstate.User;
                 if (user.Identity.IsAuthenticated)
                 {
-                    var applicationUser = await userService.GetUserByUserName(user.Identity.Name);
-                    CurrentUserGuid = applicationUser.Id;
-                    var listing = await listingService.GetListingByOwnerId(CurrentUserGuid);
-                    helper.NavigateToPageByStep(listing, Constants.CategoryComplete, navManager);
-
-                    ListingId = listing.ListingID;
-                    Steps = listing.Steps;
-                    var specialisation = await listingService.GetSpecialisationByListingId(ListingId);
-                    if (specialisation != null)
-                    {
-                        IsSpecialisationExist = true;
-                        SpecialisationVM.SetViewModel(specialisation);
-                    }
+                    
                 }
             }
             catch (Exception exc)
             {
                 string ErrorMessage = exc.Message;
-            }
-        }
-
-        public async Task SelectAllAsync()
-        {
-            SpecialisationVM.SelectOrUnselectAll(true);
-            await Task.Delay(1);
-        }
-
-        public async Task CreateSpecialisationAsync()
-        {
-            if (!SpecialisationVM.isValid())
-            {
-                helper.ShowNotification(_notice, $"Please select at least one specialisation.", NotificationType.Info);
-                return;
-            }
-
-            try
-            {
-                buttonBusy = true;
-                var specialisation = await listingService.GetSpecialisationByListingId(ListingId);
-                bool recordNotFound = specialisation == null;
-
-                if (recordNotFound)
-                    specialisation = new BOL.LISTING.Specialisation();
-
-                SpecialisationVM.SetContextModel(specialisation);
-
-                if (recordNotFound)
-                {
-                    specialisation.ListingID = ListingId;
-                    specialisation.OwnerGuid = CurrentUserGuid;
-                    specialisation.IPAddress = httpConAccess.HttpContext.Connection.RemoteIpAddress.ToString();
-
-                    await listingService.AddAsync(specialisation);
-                }
-                else
-                {
-                    await listingService.UpdateAsync(specialisation);
-                }
-
-                await listingService.UpdateListingStepByOwnerId(CurrentUserGuid, Constants.SpecialisationComplete, Steps);
-                navManager.NavigateTo($"/MyAccount/Listing/WorkingHours");
-            }
-            catch (Exception exc)
-            {
-                helper.ShowNotification(_notice, exc.Message, NotificationType.Error);
-            }
-            finally
-            {
-                buttonBusy = false;
             }
         }
     }
