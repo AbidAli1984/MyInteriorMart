f00e5e62a32a80a9ad2799377cf404e9a75f78ec
abidalisayyed786@gmail.com <abidalisayyed786@gmail.com>
2023-09-23 17:36:02 +0530

    CategoryForm component impelemented for reuseability

--- FRONTEND.BLAZOR/MyAccount/ListingWizard/Category.razor.cs
+++ FRONTEND.BLAZOR/MyAccount/ListingWizard/Category.razor.cs
@@ -1,34 +1,13 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Http;
-using AntDesign;
-using DAL.Models;
-using BAL.Services.Contracts;
-using BOL.ComponentModels.MyAccount.ListingWizard;
-using BOL;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace FRONTEND.BLAZOR.MyAccount.ListingWizard
 {
     public partial class Category
     {
-        [Inject]
-        private IHttpContextAccessor httpConAccess { get; set; }
-        [Inject]
-        public IListingService listingService { get; set; }
-        [Inject]
-        public ICategoryService categoryService { get; set; }
-        [Inject]
-        public IUserService userService { get; set; }
-        [Inject]
-        public Helper helper { get; set; }
-
-        public CategoryVM CategoryVM { get; set; } = new CategoryVM();
-        public bool buttonBusy { get; set; }
-        public string CurrentUserGuid { get; set; }
-        public int ListingId { get; set; }
-        public int Steps { get; set; }
-        public bool IsCategoryExist { get; set; }
+        [Inject] AuthenticationStateProvider authenticationState { get; set; }
 
         protected async override Task OnInitializedAsync()
         {
@@ -38,111 +17,12 @@ namespace FRONTEND.BLAZOR.MyAccount.ListingWizard
                 var user = authstate.User;
                 if (user.Identity.IsAuthenticated)
                 {
-                    var applicationUser = await userService.GetUserByUserName(user.Identity.Name);
-                    CurrentUserGuid = applicationUser.Id;
-                    var listing = await listingService.GetListingByOwnerId(CurrentUserGuid);
-                    helper.NavigateToPageByStep(listing, Constants.AddressComplete, navManager);
-
-                    ListingId = listing.ListingID;
-                    Steps = listing.Steps;
-                    CategoryVM.FirstCategories = await categoryService.GetFirstCategoriesAsync();
-                    var category = await listingService.GetCategoryByListingId(ListingId);
-                    if (category != null)
-                    {
-                        IsCategoryExist = true;
-                        CategoryVM.SetViewModel(category);
-
-                        await GetSecondCategoryIdByFirstCategoryId(null);
-                        await GetOtherCategoriesBySecondCategoryId(null);
-                    }
+                    
                 }
             }
             catch (Exception exc)
             {
                 string ErrorMessage = exc.Message;
-            }
-        }
-
-        public async Task GetSecondCategoryIdByFirstCategoryId(ChangeEventArgs events)
-        {
-            try
-            {
-                await categoryService.GetSecCategoriesByFirstCategoryId(CategoryVM, events);
-                StateHasChanged();
-            }
-            catch (Exception exc)
-            {
-                string ErrorMessage = exc.Message;
-            }
-        }
-
-        public async Task GetOtherCategoriesBySecondCategoryId(ChangeEventArgs events)
-        {
-            try
-            {
-                if (events != null)
-                {
-                    int.TryParse(events.Value.ToString(), out int secondCatId);
-                    CategoryVM.SecondCategoryID = secondCatId;
-                }
-                await categoryService.GetOtherCategoriesBySeconCategoryId(CategoryVM);
-                await Task.Delay(500);
-            }
-            catch (Exception exc)
-            {
-                string ErrorMessage = exc.Message;
-            }
-        }
-
-        public async Task SelectAllCategories()
-        {
-            categoryService.MarkAllCategoriesSelected(CategoryVM);
-            await Task.Delay(1);
-        }
-
-        public async Task CreateListingCategory()
-        {
-            if (!CategoryVM.isValid())
-            {
-                helper.ShowNotification(_notice, $"First Category and Second Category must not be blank.", NotificationType.Info);
-                return;
-            }
-
-            try
-            {
-                buttonBusy = true;
-                var category = await listingService.GetCategoryByListingId(ListingId);
-                bool recordNotFound = category == null;
-
-                if (recordNotFound)
-                    category = new BOL.LISTING.Categories();
-
-                categoryService.GetOtherCategoriesToUpdate(CategoryVM);
-                CategoryVM.SetContextModel(category);
-
-                if (recordNotFound)
-                {
-                    category.ListingID = ListingId;
-                    category.OwnerGuid = CurrentUserGuid;
-                    category.IPAddress = httpConAccess.HttpContext.Connection.RemoteIpAddress.ToString();
-
-                    await listingService.AddAsync(category);
-                }
-                else
-                {
-                    await listingService.UpdateAsync(category);
-                }
-
-                await listingService.UpdateListingStepByOwnerId(CurrentUserGuid, Constants.CategoryComplete, Steps);
-                navManager.NavigateTo($"/MyAccount/Listing/Specialisation");
-            }
-            catch (Exception exc)
-            {
-                helper.ShowNotification(_notice, exc.Message, NotificationType.Error);
-            }
-            finally
-            {
-                buttonBusy = false;
             }
         }
     }
