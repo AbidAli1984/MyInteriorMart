389298d28aa672568027999f028dfb8e412851a6
abidalisayyed786@gmail.com <abidalisayyed786@gmail.com>
2023-09-23 21:31:21 +0530

    HTML page checkin done

--- FRONTEND/HTMLS/FrontEnd/js/main.js
+++ FRONTEND/HTMLS/FrontEnd/js/main.js
@@ -139,6 +139,25 @@ $('.owl-carousel-1').owlCarousel({
     }
 });
 
+$('.owl-carousel-2').owlCarousel({
+    rtl: false,
+    loop: true,
+    margin: 15,
+    nav: false,
+    responsiveClass:true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 4
+        }
+    }
+});
+
 $('.owl-carousel').owlCarousel({
     rtl: false,
     loop: true,
@@ -156,7 +175,10 @@ $('.owl-carousel').owlCarousel({
             items: 4
         }
     }
-})
+});
+
+
+
 
 
 // --------------------- Image Upload ---------------
@@ -284,5 +306,112 @@ $('.pro-nav').slick({
     }]
 });
 
+
+$(document).ready(function() {
+    $('#bookmark').click(function() {
+        $('#v-pills-dashboard').removeClass('active');
+        $('#v-pills-dashboard').removeClass('show');
+        $('#v-pills-dashboard-tab').removeClass('active');
+        $('#v-pill-bookmark').addClass('active');
+        $('#v-pill-bookmark').addClass('show');
+    });
+
+    $('#like').click(function() {
+        $('#v-pills-dashboard').removeClass('active');
+        $('#v-pills-dashboard').removeClass('show');
+        $('#v-pills-dashboard-tab').removeClass('active');
+        $('#v-pill-like').addClass('active');
+        $('#v-pill-like').addClass('show');
+    });
+
+    $('#review').click(function() {
+        $('#v-pills-dashboard').removeClass('active');
+        $('#v-pills-dashboard').removeClass('show');
+        $('#v-pills-dashboard-tab').removeClass('active');
+        $('#v-pill-review').addClass('active');
+        $('#v-pill-review').addClass('show');
+    });
+
+    $('#subscribe').click(function() {
+        $('#v-pills-dashboard').removeClass('active');
+        $('#v-pills-dashboard').removeClass('show');
+        $('#v-pills-dashboard-tab').removeClass('active');
+        $('#v-pill-subscribe').addClass('active');
+        $('#v-pill-subscribe').addClass('show');
+    });
+});
+
+
+function validateNumber(e) {
+
+    const pattern = /^[0-9]$/;
+
+    return pattern.test(e.key)
+
+}
+
+
+
+//  -------------- Multistep Form ----------------
+
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
+
+function showTab(n) {
+  // This function will display the specified tab of the form...
+  var x = document.getElementsByClassName("tab");
+  x[n].style.display = "block";
+  //... and fix the Previous/Next buttons:
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+  } else {
+    document.getElementById("prevBtn").style.display = "inline";
+  }
+  if (n == (x.length - 1)) {
+    document.getElementById("nextBtn").innerHTML = "Submit";
+  } else {
+    document.getElementById("nextBtn").innerHTML = "Next";
+  }
+  //... and run a function that will display the correct step indicator:
+  fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+  // This function will figure out which tab to display
+  var x = document.getElementsByClassName("tab");
+  // Exit the function if any field in the current tab is invalid:
+//   if (n == 1 && !validateForm()) return false;
+  // Hide the current tab:
+  x[currentTab].style.display = "none";
+  // Increase or decrease the current tab by 1:
+  currentTab = currentTab + n;
+  // if you have reached the end of the form...
+  if (currentTab >= x.length) {
+    // ... the form gets submitted:
+    document.getElementById("regForm").submit();
+    return false;
+  }
+  // Otherwise, display the correct tab:
+  showTab(currentTab);
+}
+
+
+
+function fixStepIndicator(n) {
+  // This function removes the "active" class of all steps...
+  var i, x = document.getElementsByClassName("step");
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(" active", "");
+  }
+  //... and adds the "active" class on the current step:
+  x[n].className += " active";
+}
+
+
+
+
 // Image zoom effect
 $('.img-zoom').zoom();
+
+
+
