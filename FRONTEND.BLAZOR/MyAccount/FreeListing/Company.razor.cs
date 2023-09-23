dc3df42d37862f87cd11c009208590bb5a4b51fa
abidalisayyed786@gmail.com <abidalisayyed786@gmail.com>
2023-09-23 21:30:49 +0530

    Free listing > Add company implementation done

---
+++ FRONTEND.BLAZOR/MyAccount/FreeListing/Company.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Threading.Tasks;
+
+namespace FRONTEND.BLAZOR.MyAccount.FreeListing
+{
+    public partial class Company
+    {
+        [Inject] private AuthenticationStateProvider authenticationState { get; set; }
+        public string Url { get { return BOL.Constants.getListingUrl(BOL.Constants.CreateListing) + "/Company";  } }
+
+        protected async override Task OnInitializedAsync()
+        {
+            try
+            {
+                var authstate = await authenticationState.GetAuthenticationStateAsync();
+                var user = authstate.User;
+                if (user.Identity.IsAuthenticated)
+                {
+
+                }
+            }
+            catch (Exception exc)
+            {
+                string ErrorMessage = exc.Message;
+            }
+        }
+    }
+}
