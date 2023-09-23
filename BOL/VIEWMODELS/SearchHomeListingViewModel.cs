3bad6f6e2fc3fac1072f84fe620579fab96e137a
abidalisayyed786@gmail.com <abidalisayyed786@gmail.com>
2023-09-23 17:10:39 +0530

    missing home page search view model checkin

---
+++ BOL/VIEWMODELS/SearchHomeListingViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOL.VIEWMODELS
+{
+    public class SearchHomeListingViewModel
+    {
+        public string Id { get; set; }
+        public string CompanyName { get; set; }
+        public string ListingUrl { get; set; }
+        public string CityName { get; set; }
+        public string LocalityName { get; set; }
+    }
+}
