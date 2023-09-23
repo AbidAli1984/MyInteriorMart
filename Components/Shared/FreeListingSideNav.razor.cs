4f2875d48c431f69afe817538f33ec7b41382a64
abidalisayyed786@gmail.com <abidalisayyed786@gmail.com>
2023-09-23 19:03:25 +0530

    Create Free Listing Page added

---
+++ Components/Shared/FreeListingSideNav.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Components.Shared
+{
+    public partial class FreeListingSideNav
+    {
+        [Parameter]
+        public bool isVendor { get; set; }
+
+        [Parameter]
+        public int currentPage { get; set; }
+
+        public string ActiveLink(int pageId)
+        {
+            return currentPage == pageId ? "active" : "";
+        }
+    }
+}
---
+++ FRONTEND.BLAZOR/MyAccount/CreateFreeListing/CreateListing.razor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FRONTEND.BLAZOR.MyAccount.VendorListing
+{
+    public partial class CreateListing
+    {
+    }
+}
