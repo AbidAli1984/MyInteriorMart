209a197a38af7642055f0a9143736501472ba522
abidalisayyed786@gmail.com <abidalisayyed786@gmail.com>
2023-09-23 21:47:27 +0530

    Free listing > Add Category implementation done

---
+++ FRONTEND.BLAZOR/MyAccount/FreeListing/Category.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Threading.Tasks;
+
+namespace FRONTEND.BLAZOR.MyAccount.FreeListing
+{
+    public partial class Category
+    {
+        [Inject] private AuthenticationStateProvider authenticationState { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            try
+            {
+                var authstate = await authenticationState.GetAuthenticationStateAsync();
+                var user = authstate.User;
+                if (user.Identity.IsAuthenticated)
+                {
+
+                }
+            }
+            catch (Exception exc)
+            {
+                string ErrorMessage = exc.Message;
+            }
+        }
+    }
+}
